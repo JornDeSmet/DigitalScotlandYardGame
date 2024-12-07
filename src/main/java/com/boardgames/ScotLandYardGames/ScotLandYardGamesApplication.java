aa081a64d4647d2d95d20e2550c0521fd1433190
jornDS <117446313+JornDeSmet@users.noreply.github.com>
2024-12-07 16:01:17 +0100

    Initial commit

---
+++ src/main/java/com/boardgames/ScotLandYardGames/ScotLandYardGamesApplication.java
@@ -0,0 +1,13 @@
+package com.boardgames.ScotLandYardGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScotLandYardGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScotLandYardGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardgames/ScotLandYardGames/ScotLandYardGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgames.ScotLandYardGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScotLandYardGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
